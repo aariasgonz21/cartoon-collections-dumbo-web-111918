069c0bd3abedc99bfc792b377708529b7c81ff0d
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-10 04:56:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,8 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese(srray)
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  if (array.include)
 end
